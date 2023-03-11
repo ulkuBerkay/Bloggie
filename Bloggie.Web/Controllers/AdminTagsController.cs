9e39accfd1ad69063953b60b34a2bd37e951f8b8
berkayulku55 <berkayulku55@icloud.com>
2023-03-11 16:13:34 +0300

    First Data Added.

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -1,4 +1,6 @@
-﻿using Bloggie.Web.Models.ViewModels;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
@@ -6,10 +8,34 @@ namespace Bloggie.Web.Controllers
 
     public class AdminTagsController : Controller
     {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        //private BloggieDbContext _bloggieDbContext;
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //var name = addTagRequest.Name;
+            //var display = addTagRequest.DisplayName;
+
+            var tag = new Tag { 
+            Name = addTagRequest.Name,
+            DisplayName = addTagRequest.DisplayName
+            };
+            bloggieDbContext.Add(tag);
+            bloggieDbContext.SaveChanges();
+            return View("Add");
         }
 
         //[HttpPost]
@@ -20,14 +46,5 @@ namespace Bloggie.Web.Controllers
         //    //var displayName = Request.Form["displayName"];
         //    return View("Add");
         //}
-
-        [HttpPost]
-        [ActionName("Add")]
-        public IActionResult Add(AddTagRequest addTagRequest)
-        {
-            var name = addTagRequest.Name;
-            var display = addTagRequest.DisplayName;
-            return View("Add");
-        }
     }
 }
