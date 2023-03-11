cebf59ade8528c9742247bcf46a3905186e95c3e
berkayulku55 <berkayulku55@icloud.com>
2023-03-11 14:59:43 +0300

    AdminPanel Created. AddTags created with form.

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag()
+        {
+            var name = Request.Form["name"];
+            var displayName = Request.Form["displayName"];
+            return View("Add");
+        }
+    }
+}
