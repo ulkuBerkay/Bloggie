e3639586d59a5cf6ec469257c2c2479c112ccd16
berkayulku55 <berkayulku55@icloud.com>
2023-03-11 12:17:05 +0300

    EntitiesAdded

---
+++ Bloggie.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Bloggie.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PubishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
