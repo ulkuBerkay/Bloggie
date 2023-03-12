c4044da58471d2f2346d7af5baadfe0406b4ba3c
berkayulku55 <berkayulku55@icloud.com>
2023-03-12 16:33:41 +0300

    AdminBlogPostController, View,ViewModel created. Add Method started.

---
+++ Bloggie.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,30 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagInterface tagRepository;
+
+        public AdminBlogPostsController(ITagInterface tagRepository)
+        {
+            this.tagRepository = tagRepository;
+        }
+        [HttpGet]
+        public async Task <IActionResult> Add()
+        {
+            var tags = await tagRepository.GetAllAsync();
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Bloggie.Web/Models/ViewModels/AddBlogPostRequest.cs
@@ -0,0 +1,26 @@
+﻿using Bloggie.Web.Models.Domain;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bloggie.Web.Models.ViewModels
+{
+    public class AddBlogPostRequest
+    {
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PubishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+        
+        //Listing tags from dB
+        public IEnumerable<SelectListItem> Tags { get; set; }
+
+        //For chosen tag
+        public string SelectedTag { get; set; }
+
+    }
+}
