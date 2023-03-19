156dd196f983266e286d4fd7fb57b917e7526ece
berkayulku55 <berkayulku55@icloud.com>
2023-03-19 12:34:18 +0300

    BlogPostLike added to Domain.

--- Bloggie.Web/Models/Domain/BlogPost.cs
+++ Bloggie.Web/Models/Domain/BlogPost.cs
@@ -15,5 +15,6 @@ namespace Bloggie.Web.Models.Domain
         public string Author { get; set; }
         public bool Visible { get; set; }
         public ICollection<Tag> Tags { get; set; }
+        public ICollection<BlogPostLike> Likes { get; set; }
     }
 }
